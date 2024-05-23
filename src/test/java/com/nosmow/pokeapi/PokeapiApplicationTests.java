22f36dd6908d34a0aee44cd29613f9b81f3b42da
nosmow <nosmow.dev@gmail.com>
2024-05-23 16:12:50 -0500

    Initial configuration

---
+++ src/test/java/com/nosmow/pokeapi/PokeapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nosmow.pokeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokeapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
